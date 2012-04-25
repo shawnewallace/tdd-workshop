3968ef0e7550b8996381e849d93dea85096b1888
Shawn Wallace <shawn@wshawn.com>
2012-04-25 09:09:27 -0400

    added skeleton for map and presentations

---
+++ maps/Centric Consulting TDD Workshop.mm
@@ -0,0 +1,4 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1335359199062" ID="ID_1205665368" MODIFIED="1335359211642" TEXT="Centric Consulting TDD Workshop"/>
+</map>
