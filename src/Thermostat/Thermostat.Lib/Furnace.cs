c3257495751a6f3678dd7354dfefa28c136c580a
Shawn Wallace <shawnewallace@gmail.com>
2013-04-22 10:58:32 -0400

    Added Thermostat example for DIP

---
+++ src/Thermostat/Thermostat.Lib/Furnace.cs
@@ -0,0 +1,49 @@
+﻿using System.Diagnostics;
+
+namespace Thermostat.Lib.First
+{
+	public class Furnace
+	{
+		public Furnace()
+		{
+			off();
+		}
+
+		public void on()
+		{
+			Debug.WriteLine("Furnace is ON");
+		}
+
+		public void off()
+		{
+			Debug.WriteLine("Furnace is OFF");
+		}
+	}
+
+	public class Thermostat
+	{
+		private Furnace _furnace;
+
+		public Thermostat(Furnace furnace)
+		{
+			_furnace = furnace;
+		}
+
+		public void run()
+		{
+			if (shouldBeOn())
+			{
+				_furnace.on();
+			}
+			else
+			{
+				_furnace.off();
+			}
+		}
+
+		private bool shouldBeOn()
+		{
+			return true;
+		}
+	}
+}
---
+++ src/Thermostat/Thermostat.Lib/FurnaceWithInterface.cs
@@ -0,0 +1,55 @@
+﻿using System.Diagnostics;
+
+namespace Thermostat.Lib.Second
+{
+	public interface ISwitchableDevice
+	{
+		void on();
+		void off();
+	}
+
+	public class Thermostat
+	{
+		private ISwitchableDevice _device;
+
+		public Thermostat(ISwitchableDevice device)
+		{
+			_device = device;
+		}
+
+		public void run()
+		{
+			if (shouldBeOn())
+			{
+				_device.on();
+			}
+			else
+			{
+				_device.off();
+			}
+		}
+
+		private bool shouldBeOn()
+		{
+			return true;
+		}
+	}
+
+	public class Furnace
+	{
+		public Furnace()
+		{
+			off();
+		}
+
+		public void on()
+		{
+			Debug.WriteLine("Furnace is ON");
+		}
+
+		public void off()
+		{
+			Debug.WriteLine("Furnace is OFF");
+		}
+	}
+}
